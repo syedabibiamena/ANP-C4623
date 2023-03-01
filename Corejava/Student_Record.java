986070343b3f09ad64a48ac242b8c963b8f7116e
syedabibiamena <87406704+syedabibiamena@users.noreply.github.com>
2023-03-01 17:40:25 +0530

    Add files via upload

---
+++ Corejava/Student_Record.java
@@ -0,0 +1,18 @@
+package corejava1;
+
+public class Student_Record
+{
+
+	public static void main(String[] args)
+	{
+		//creating object of class student
+		 Student a=new Student();
+		// object is used to access the methods of a class 
+		 a.setId(1);
+		 a.setStudent_name("Aamena");
+		 a.setAge(10);
+		 System.out.println("Student Data");
+		 System.out.println("Id:"+a.getId()+"\n"+"Student Name:" +a.getStudent_name()+"\n"+"Student Age:"+a.getAge());
+		 
+}
+	}
