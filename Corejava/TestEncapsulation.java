32f5d560ae5067a3ebc270ded35fe10086bf000a
syedabibiamena <87406704+syedabibiamena@users.noreply.github.com>
2023-03-01 16:41:52 +0530

    Add files via upload

---
+++ Corejava/TestEncapsulation.java
@@ -0,0 +1,17 @@
+package corejava1;
+
+public class TestEncapsulation {
+
+	public static void main(String[] args)
+	{
+//creating object of class EncapsulationExample
+	 EncapsulationExample a=new EncapsulationExample();
+	 //object is used access the methods of a class 
+	 a.setId(1);
+	 a.setName("Aamena,");
+	 System.out.println("id:"+a.getId()+"\n"+"Name:" +a.getName());
+	 
+
+	}
+
+}
