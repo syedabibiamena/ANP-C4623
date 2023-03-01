4fe4cafa36ce0f4ec3152adf5e53bca20fb3b584
syedabibiamena <87406704+syedabibiamena@users.noreply.github.com>
2023-03-01 17:23:32 +0530

    Add files via upload

---
+++ Corejava/Student.java
@@ -0,0 +1,27 @@
+package corejava1;
+
+class Student 
+{
+
+		 private int Id;
+		 private String student_name;
+		 private int age;
+		public int getId() {
+			return Id;
+		}
+		public void setId(int id) {
+			Id = id;
+		}
+		public String getStudent_name() {
+			return student_name;
+		}
+		public void setStudent_name(String student_name) {
+			this.student_name = student_name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+}
